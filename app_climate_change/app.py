ae43f693712df713e3306c5056aafac4d5bd97d8
Georgios Fotakis <fotakisg@gmail.com>
2025-07-04 16:25:57 +0300

    climate data prep

--- app_climate_change/app.py
+++ app_climate_change/app.py
@@ -1,4 +1,17 @@
 from shiny import App, render, ui
+import pandas as pd
+from pathlib import Path
+
+# Import dataset
+temperatures = pd.read_csv(Path(__file__).parent / "temperatures.csv")
+
+# Define values for drop-down menu
+countries = temperatures['Country'].unique().tolist()
+
+# Define values for slider
+temp_years = temperatures['Year'].unique()
+temp_year_min = temp_years.min()
+temp_year_max = temp_years.max()
 
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
