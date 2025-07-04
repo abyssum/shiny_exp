cf0fcd34083242233cfab5fa2b10ac0849703194
Georgios Fotakis <fotakisg@gmail.com>
2025-07-04 12:47:57 +0300

    update reactive calculations

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
-from shiny import App, render, ui
+from shiny import App, render, ui, reactive
 
 app_ui = ui.page_fluid(
-    ui.h2("Password"),
-    # ui.input_slider(id="n",
-    #                 label="N",
-    #                 min=0,
-    #                 max=100,
-    #                 value=[20, 30],
-    #                 animate=True),
-    # ui.input_checkbox(id = "check", 
-    #                   label = "Check", value=True),
-    # ui.input_numeric(id = "num", 
-    #                   label = "Num",
-    #                   min=10, max=30, value=25, step=5),
-    # ui.input_radio_buttons(id="ch_grp",
-    #                         label="Choice Boxes",
-    #                         choices=["Choise1", "Choise2"], selected="Choise1"),
-    ui.input_date_range(id="date",
-                  label="Choice Date"),
-    # ui.input_password(id="txt_inp",
-    #              label="TXT input"),
-    ui.h2("Show password"),
-    ui.input_checkbox(id="ch_bx",
-                      label="Show password"),
-    ui.output_text_verbatim("txt")
+    ui.h2("Reacitvity"),
+    ui.input_slider(id="n",
+                    label="N",
+                    min=0,
+                    max=100,
+                    value=20),
+    ui.output_text_verbatim("result1"),
+    ui.output_text_verbatim("result2"),
 )
 
 
 def server(input, output, session):
+    @reactive.Calc
+    def reactive_n():
+        return input.n()
+
+    def reactive_result(mult):
+        return f"n*{mult} is {reactive_n() * mult}"
+
     @output
     @render.text
-    def txt():
-        return f"{input.txt_inp()}"
+    def result1():
+        return reactive_result(2)
+    
+    @output
+    @render.text
+    def result2():
+        return reactive_result(5)
 
 
 app = App(app_ui, server)
