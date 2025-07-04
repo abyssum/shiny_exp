635cc854feee909b5de02a026165f7b4f9539a97
Georgios Fotakis <fotakisg@gmail.com>
2025-07-04 16:55:53 +0300

    frontend add rows-cols

--- app_climate_change/app.py
+++ app_climate_change/app.py
@@ -1,6 +1,7 @@
 from shiny import App, render, ui
 import pandas as pd
 from pathlib import Path
+from shinywidgets import output_widget, register_widget, reactive_read
 
 # Import dataset
 temperatures = pd.read_csv(Path(__file__).parent / "temperatures.csv")
@@ -18,6 +19,35 @@ font_style = 'font-wight: 100'
 
 app_ui = ui.page_fluid(
     ui.h2("Climate Change", style=font_style),
+    ui.row(
+        ui.column(
+            6, ui.input_select(
+                id='country',
+                label='Choose a country',
+                choices=countries
+            )
+        ),
+        ui.column(
+            6, 
+            ui.row(
+                ui.input_slider(
+                    id='year',
+                    label='Choose a year',
+                    min=temp_year_min,
+                    max=temp_year_max,
+                    value=temp_year_min,
+                    step=1,
+                    animate=False
+                ),
+                ui.column(6, ui.output_ui('color_map'))
+            )
+        )
+    ),
+    ui.row(
+        ui.column(6, ui.output_plot('graph_country')),
+        ui.column(6, output_widget('map')),
+    ),
+    ui.br(),
 )
 
 
@@ -25,7 +55,7 @@ def server(input, output, session):
     @output
     @render.text
     def txt():
-        return f"n*2 is {input.n() * 2}"
+        pass
 
 
 app = App(app_ui, server)
