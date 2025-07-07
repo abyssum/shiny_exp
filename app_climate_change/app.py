5d5ecad3c41b37e8955e91fdee006f92aeb76e2f
Georgios Fotakis <fotakisg@gmail.com>
2025-07-07 10:42:40 +0300

    update forntend - layout

--- app_climate_change/app.py
+++ app_climate_change/app.py
@@ -15,7 +15,7 @@ temp_year_min = temp_years.min()
 temp_year_max = temp_years.max()
 
 # CSS defs
-font_style = 'font-wight: 100'
+font_style = 'font-weight: 100'
 
 app_ui = ui.page_fluid(
     ui.h2("Climate Change", style=font_style),
