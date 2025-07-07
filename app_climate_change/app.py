d8fa9729495aef2d6fe852be26e5195df3b495ff
Georgios Fotakis <fotakisg@gmail.com>
2025-07-07 11:44:11 +0300

    update frontend - static content

--- app_climate_change/app.py
+++ app_climate_change/app.py
@@ -1,61 +1,78 @@
-from shiny import App, render, ui
+from shiny import App, render, ui, reactive
 import pandas as pd
 from pathlib import Path
 from shinywidgets import output_widget, register_widget, reactive_read
+import ipyleaflet as L
+from plot_funs import plot_country, plot_world
+from asyncio import sleep
 
-# Import dataset
-temperatures = pd.read_csv(Path(__file__).parent / "temperatures.csv")
+temperatures = pd.read_csv(Path(__file__).parent / 'temperatures.csv')
 
-# Define values for drop-down menu
+# values for dropdown
 countries = temperatures['Country'].unique().tolist()
 
-# Define values for slider
+# values for slider
 temp_years = temperatures['Year'].unique()
 temp_year_min = temp_years.min()
 temp_year_max = temp_years.max()
 
-# CSS defs
-font_style = 'font-weight: 100'
+# CSS 
+font_style = "font-weight: 100"
 
 app_ui = ui.page_fluid(
-    ui.h2("Climate Change", style=font_style),
+    ui.h2("Climate Change", style = font_style),
     ui.row(
-        ui.column(
-            6, ui.input_select(
-                id='country',
-                label='Choose a country',
-                choices=countries
-            )
-        ),
-        ui.column(
-            6, 
-            ui.row(
-                ui.input_slider(
-                    id='year',
-                    label='Choose a year',
-                    min=temp_year_min,
-                    max=temp_year_max,
-                    value=temp_year_min,
-                    step=1,
-                    animate=False
-                ),
-                ui.column(6, ui.output_ui('color_map'))
-            )
-        )
-    ),
+        ui.column(6, ui.input_select(id="country", label="Choose a country", choices=countries)),
+        ui.column(6, 
+                  ui.row(
+                    ui.column(6, ui.input_slider(id="year", label="Choose a Year", min=temp_year_min, max=temp_year_max, value=temp_year_min, step=1, animate=False)),
+                    ui.column(6, ui.output_ui('color_map'))
+                      
+                  )
+    )
+),
     ui.row(
         ui.column(6, ui.output_plot('graph_country')),
         ui.column(6, output_widget('map')),
     ),
     ui.br(),
+    ui.row(
+        ui.column(6, ui.h5("Imprint", style =font_style)),
+        ui.column(6, ui.p(f"Learn how this app is developed and deployed in my course:", style=font_style))
+    ),
+    ui.row(
+        ui.column(6, ui.row(
+            ui.column(2, ui.img(src="developer.png", width="32px"), style="text-align: center;"),
+            ui.column(10, ui.p("Georgios Fotakis", style=font_style)),
+        ),
+                  ui.row(
+                ui.column(2, ui.img(src="address2.png", width="32px"), style="text-align: center;"),
+                ui.column(10, ui.p('OmicaNexus.ai', style = font_style))
+                ),ui.row(
+                ui.column(2, ),
+                ui.column(10, ui.p('11474 Athens', style = font_style))
+                ),
+                ui.row(
+                ui.column(2, ),
+                ui.column(10, ui.p('Greece', style = font_style))
+                ),
+                      ui.row(
+                ui.column(2, ui.img(src="mail2.png", width="32px"), style="text-align: center;"),
+                ui.column(10, ui.a(ui.p('fotakisg@gmail.com'), href="mailto:fotakisg@gmail.com", style = font_style))
+                ),
+                  
+                  ),
+        ui.column(6, ui.a(ui.img(src="course_logo_300x169.png"), href="https://www.udemy.com"), style="text-align:center;")
+    ), 
+    ui.p("Data Source: Sevgi Sy. Temperature Change, 2022", ui.a("Kaggle Dataset", href= "https://www.kaggle.com/datasets/sevgisarac/temperature-change"), ", ",ui.a("License: Attribution 3.0 IGO (CC BY 3.0 IGO)", href="https://creativecommons.org/licenses/by/3.0/igo/"), style = font_style),
+    style="background-color:#fff"
 )
 
 
 def server(input, output, session):
-    @output
-    @render.text
-    def txt():
-        pass
+    pass
+    
 
 
-app = App(app_ui, server)
+www_dir = Path(__file__).parent / "www"
+app = App(app_ui, server, static_assets=www_dir)
